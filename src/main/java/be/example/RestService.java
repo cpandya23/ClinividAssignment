7d57920ce6c817ae736aef346d59d0e26430dd09
chintan_pc <chintan_pc@chintan>
2018-02-03 20:17:37 +0530

    Bug fixing

--- src/main/java/be/example/RestService.java
+++ src/main/java/be/example/RestService.java
@@ -17,50 +17,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class RestService {
 
-	@RequestMapping(value="/getJson",method=RequestMethod.POST)
-	
-	public JsonResponse getJsonResponse(@RequestBody String input){
-		
-		JsonResponse jsonResponse= new JsonResponse();
+	@RequestMapping(value = "/getJson", method = RequestMethod.POST)
 
-		Name name= new Name();
+	public JsonResponse getJsonResponse(@RequestBody String input) {
+
+		JsonResponse jsonResponse = new JsonResponse();
+
+		Name name = new Name();
 		Set<Location> locations = new HashSet<>();
-		
-		String[] inputArray= input.split("\\|");
+		input=input+".img";
+		System.out.println(input);
+		String[] inputArray = input.split("\\|");
 		jsonResponse.setId(inputArray[1]);
-		
-		String[] nameArray =inputArray[2].replaceAll("\\<", "").replaceAll("\\>", " ").split(" ");
+
+		String[] nameArray = inputArray[2].replaceAll("\\<>","<null>").replaceAll("\\<", "").replaceAll("\\>", " ")
+				.split(" ");
+
 		name.setFirst(nameArray[0]);
 		name.setMiddle(nameArray[1]);
 		name.setLast(nameArray[2]);
 		jsonResponse.setName(name);
-		jsonResponse.setDob(inputArray[3].replaceAll("\\<", "").replaceAll("\\>", ""));
-		jsonResponse.setImageId(inputArray[5]);
-		
-		String locArray[] =inputArray[4].replaceAll("\\<", "").replaceAll("\\>", " ").split(",");
-		
-		for(int i=0;i<locArray.length;i++) {
-			
-			String locDetails[]=locArray[i].split(" ");
-			Location location= new Location();
-			Coords coords= new Coords();
-			location.setName(locDetails[0]);
-			coords.setLat(Double.parseDouble(locDetails[1]));
-			coords.setLong(Double.parseDouble(locDetails[2]));
-			
-			location.setCoords(coords);
-			
-			locations.add(location);
-		
+		if(inputArray[3].equals("")) {
+			inputArray[3]="null";
 		}
-		for(Location x: locations) {
+		jsonResponse.setDob(inputArray[3]);
+		if(inputArray[5].substring(0, inputArray[5].lastIndexOf(".")).equals("")){
+			jsonResponse.setImageId("null");
+		}
+		else {
+			jsonResponse.setImageId(inputArray[5].substring(0, inputArray[5].lastIndexOf(".")));
+		}
+		
+
+
+		String locArray[] = inputArray[4].replaceAll("\\<>", "<null>").replaceAll("\\<", "").replaceAll("\\>", " ")
+				.split(",");
+
+		for (int i = 0; i < locArray.length; i++) {
+			System.out.println(locArray[i]);
+			String locDetails[] = locArray[i].split(" ");
+			Location location = new Location();
+
+			Coords coords = new Coords();
+
+			location.setName(locDetails[0]);
+			coords.setLat(locDetails[1]);
+			coords.setLong(locDetails[2]);
+
+			location.setCoords(coords);
+
+			locations.add(location);
+
+		}
+		for (Location x : locations) {
 			System.out.println(x.getName());
 		}
-		List<Location> listLoc=locations.stream().map(t->t).collect(Collectors.toList());
+		List<Location> listLoc = locations.stream().map(t -> t).collect(Collectors.toList());
 		jsonResponse.setLocations(listLoc);
 		return jsonResponse;
-			
-	}
-	
-}
 
+	}
+
+}
