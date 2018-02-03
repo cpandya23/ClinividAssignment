db073424a07820de017b885982a35e5ffc94f1b5
chintan_pc <chintan_pc@chintan>
2018-02-03 19:13:49 +0530

    Bug resolved.

--- src/main/java/be/example/RestService.java
+++ src/main/java/be/example/RestService.java
@@ -1,18 +1,16 @@
 package be.example;
 
-import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import be.example.Coords;
-import be.example.JsonResponse;
-import be.example.Location;
-import be.example.Name;
 
 @RestController
 @RequestMapping("rest")
@@ -26,9 +24,8 @@ public class RestService {
 		JsonResponse jsonResponse= new JsonResponse();
 
 		Name name= new Name();
-		List<Location> locations = new ArrayList<>();
-		Location location= new Location();
-		Coords coords= new Coords();
+		Set<Location> locations = new HashSet<>();
+		
 		String[] inputArray= input.split("\\|");
 		jsonResponse.setId(inputArray[1]);
 		
@@ -41,15 +38,26 @@ public class RestService {
 		jsonResponse.setImageId(inputArray[5]);
 		
 		String locArray[] =inputArray[4].replaceAll("\\<", "").replaceAll("\\>", " ").split(",");
-		for(String x:locArray) {
-			String locDetails[]=x.split(" ");
+		
+		for(int i=0;i<locArray.length;i++) {
+			
+			String locDetails[]=locArray[i].split(" ");
+			Location location= new Location();
+			Coords coords= new Coords();
 			location.setName(locDetails[0]);
 			coords.setLat(Double.parseDouble(locDetails[1]));
 			coords.setLong(Double.parseDouble(locDetails[2]));
+			
 			location.setCoords(coords);
+			
 			locations.add(location);
+		
 		}
-		jsonResponse.setLocations(locations);
+		for(Location x: locations) {
+			System.out.println(x.getName());
+		}
+		List<Location> listLoc=locations.stream().map(t->t).collect(Collectors.toList());
+		jsonResponse.setLocations(listLoc);
 		return jsonResponse;
 			
 	}
